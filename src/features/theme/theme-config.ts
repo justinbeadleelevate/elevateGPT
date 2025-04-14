e7b78146e19278fae150f7f8eccd66df924f7db5
justinbeadleelevate <77413737+justinbeadleelevate@users.noreply.github.com>
2025-04-14 15:21:13 -0700

    Push 2

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,7 @@ export const AI_NAME = "Azure Chat";
 export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are NOT a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are the most friendly ${AI_NAME} AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
